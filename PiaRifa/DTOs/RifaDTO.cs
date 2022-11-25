81b4c09e0a37157c3c3db601523db4ecef28f5ed
Luis Cortez <luis_gonzalez.95@hotmail.com>
2022-11-25 13:06:54 -0600

    entidad numrifa

---
+++ PiaRifa/DTOs/RifaDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApiRifa.Validaciones;
+
+namespace WebApiRifa.DTOs
+{
+    public class RifaDTO
+    {
+        [Required(ErrorMessage = "El Nombre es requerido.")]
+        [StringLength(maximumLength: 30, ErrorMessage = "Solo pueden ser 30 caracteres.")]
+        [PrimeraLetraMayuscula]
+        public string Nombre { get; set; }
+    }
+}
