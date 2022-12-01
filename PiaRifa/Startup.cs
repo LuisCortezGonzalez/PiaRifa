f0dc0736aec0088560c122ac34feed21d215a51b
Luis Cortez <luis_gonzalez.95@hotmail.com>
2022-11-30 21:03:52 -0600

    Aut

--- PiaRifa/Startup.cs
+++ PiaRifa/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using System.IdentityModel.Tokens.Jwt;
+using WebApiRifa.Services;
 
 namespace WebApiRifa
 {
